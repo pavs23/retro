97a229afbcf7fe77730f38cae6cf1b5f13eeff04
Wilhelm Eek <richard.eek@gmail.com>
2017-10-23 17:28:41 +1100

    Test to see if pushing works from this laptop

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const appFactory = require('./app-factory')
 const app = appFactory.getAppFromEnvironment();
 const port = process.env.PORT || 5000;
+
 console.log("Now listening on " + port + " in " + app.get('env') + " mode.")
 app.listen(port)
