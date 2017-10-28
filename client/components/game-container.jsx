d55bfc56428a93dfc9854fd97d7b04df215a7c54
Wilhelm Eek <richard.eek@gmail.com>
2017-10-28 18:01:30 +1100

    Move stateless styled components out of render to avoid unnecessary redraws

--- client/components/game-container.jsx
+++ client/components/game-container.jsx
@@ -1,64 +1,57 @@
 import React from 'react'
 import styled from 'styled-components'
  
+const Container = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`
+      
+const GameId = styled.span`
+  font-weight: 400;
+`
+
+const GameIndicator = styled.div`
+  top: 12px;
+  right: 22px;
+  font-size: 18px;
+  font-weight: 700;
+  position: fixed; 
+  cursor: pointer;
+  user-select: none;
+`
+
+const NameField = styled.input`
+  top: 12px;
+  left: 22px;
+  font-size: 18px;
+  font-weight: 700;
+  position: fixed; 
+`
+
 export default class GameContainer extends React.Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      name: null
+      name: ""
     }
   }
 
-  startRename(e) {
-    this.setState({
-      ...this.state,
-      name: "Wilhelm"
-    })
+  handleChange(event) {
+    this.setState({name: event.target.value});
   }
 
-  render() {
-    const Container = styled.div`
-      position: relative;
-      width: 100%;
-      height: 100%;
-    `
-      
-    const GameId = styled.span`
-      font-weight: 400;
-    `
-
-    const GameIndicator = styled.div`
-      top: 12px;
-      right: 22px;
-      font-size: 18px;
-      font-weight: 700;
-      position: fixed; 
-      cursor: pointer;
-      user-select: none;
-    `
-
-    const NameField = styled.input`
-      top: 12px;
-      left: 22px;
-      font-size: 18px;
-      font-weight: 700;
-      position: fixed; 
-    `
-
-    const NameEntry = NameField.extend`
-      color: #999;
-    `
-    
+  render() {    
     return (
       <Container>
         <GameIndicator>GameId: <GameId>#dsa87t7</GameId> </GameIndicator>
-        {
-          this.state.name != null 
-            ? <NameField value={this.state.name}/>
-            : <NameEntry   defaultValue={"Enter Name"} 
-                onClick={this.startRename.bind(this)}/>
-        }
+        <NameField
+          key="123"
+          placeholder={"Enter Name"}
+          value={this.state.name}
+          onChange={this.handleChange.bind(this)}
+        />
         {this.props.children}
       </Container>
     )
--- client/components/landing.jsx
+++ client/components/landing.jsx
@@ -2,6 +2,57 @@ import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
+const Container = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`
+
+const WelcomeMessage = styled.p`
+  position: relative;
+  color: black;
+  text-align: center;
+  font-size: 64px;
+`
+
+const GetStartedButton = styled.button`
+  position: absolute;
+  bottom: 0px;
+  left: 50%;
+  transform: translate(-50%, 0);
+  width: 200px;
+  height: 40px;
+  border: none;
+  cursor: pointer;
+  color: #007AFF;
+  background-color: #fff;
+  text-align: center;
+  text-decoration: none;
+  font-size: 22px;
+`
+
+const GamesList = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 200px;
+`
+
+const Game = styled(Link)`
+  width: 100%;
+  margin: 15px;
+  font-size: 32px;
+  color: #007AFF;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  color: 
+`
+
+const DisabledGame = Game.extend`
+  cursor: default;
+  color: #999;
+`
+
 export default class Landing extends React.Component {
 
   constructor(props) {
@@ -13,63 +64,12 @@ export default class Landing extends React.Component {
 
   render() {
 
-    const Container = styled.div`
-      position: relative;
-      width: 100%;
-      height: 100%;
-    `
-
     const WelcomeBox = styled.div`
       position: absolute;
       width: 100%;      
       top: ${ this.state.started ? '0%' : '40%'};
     `
-
-    const WelcomeMessage = styled.p`
-      position: relative;
-      color: black;
-      text-align: center;
-      font-size: 64px;
-    `
-
-    const GetStartedButton = styled.button`
-      position: absolute;
-      bottom: 0px;
-      left: 50%;
-      transform: translate(-50%, 0);
-      width: 200px;
-      height: 40px;
-      border: none;
-      cursor: pointer;
-      color: #007AFF;
-      background-color: #fff;
-      text-align: center;
-      text-decoration: none;
-      font-size: 22px;
-    `
-
-    const GamesList = styled.div`
-      display: flex;
-      flex-direction: column;
-      margin-top: 200px;
-    `
-
-    const Game = styled(Link)`
-      width: 100%;
-      margin: 15px;
-      font-size: 32px;
-      color: #007AFF;
-      cursor: pointer;
-      text-align: center;
-      text-decoration: none;
-      color: 
-    `
-
-    const DisabledGame = Game.extend`
-      cursor: default;
-      color: #999;
-    `
-
+    
     return (
       <Container>
         <WelcomeBox>
